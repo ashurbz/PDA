05525ce8f41d49aaa8e4f8ac055860c6f37c6dd5
Ashish <rbzashu1@gmail.com>
2023-05-13 11:33:13 +0530

    Added description and completed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import "./index.css";
 function App() {
   return (
     <div className="card-container">
-      <ProfileCard title="Alexa" handle="@alexa99" image={ALEXA_IMG_URL} />
+      <ProfileCard
+        title="Alexa"
+        handle="@alexa99"
+        image={ALEXA_IMG_URL}
+        description="Alexa is from Amazon and is very famous"
+      />
       <ProfileCard
         title="Cortana"
         handle="@cortana540"
         image={CORTANA_IMG_URL}
+        description="Cortana is from Microsoft and is used very less"
       />
-      <ProfileCard title="Siri" handle="@siri555" image={SIRI_IMG_URL} />
+      <ProfileCard
+        title="Siri"
+        handle="@siri555"
+        image={SIRI_IMG_URL}
+        description="Siri is from Apple and is in its out phase"
+      />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,12 @@
 import "./index.css";
 
-const ProfileCard = ({ title, handle, image }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   return (
     <div className="card">
       <img className="image" src={image} alt="pda" />
       <span className="title">{title}</span>
       <span className="handle">{handle}</span>
+      <p>{description}</p>
     </div>
   );
 };
